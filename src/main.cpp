1f5ba024b73cb0add4d76be8594f9b7e880b9e5c
Quinten Perquin <quinten.perquin@nixu.com>
2018-05-16 12:09:55 +0200

    Changing print to vector

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "IO_Interface.hpp"
 #include "Navigation_Engine.hpp"
 #include "wrap-hwlib.hpp"
+#include "Vector2f.hpp"
 
 int main() {
     WDT->WDT_MR = WDT_MR_WDDIS;
     NavigationEngine engine;
     IOInterface interface(engine);
     hwlib::wait_ms(1000);
-    hwlib::cout << "Hello world!" << hwlib::endl;
+	Vector2f point1(1.5, 5.6);
+	std::cout << point1.x << ',' << point1.y << std::endl;
 }
