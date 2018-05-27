3dd321e6f60c79b3626f5f1a2c75c0355659ae4f
Quinten Perquin <quinten.perquin@student.hu.nl>
2018-05-27 11:39:25 +0200

    adding doxygen to navigation_path

--- src/navigation_path.hpp
+++ src/navigation_path.hpp
@@ -13,9 +13,41 @@
 
 class NavigationPath {
   public:
+    /**
+     * @brief creates a NavigationPath object.
+     *
+     * Constructor for a NavigationPath object without arguments.
+     *
+     * @return a NavigationPath object.
+     */
     NavigationPath();
+	
+	/**
+     * @brief adds a Vector2f to the path.
+     * 
+	 * adds a Vector2f to the back of the current path.
+     *
+     * @param[in]    location    a Vector2f signifying a location
+     * @return void
+     */
     void addPathLocation(Vector2f location);
+	
+    /**
+     * @brief gets the next location from the path.
+     * 
+	 * gets the Vector2f at the front of the current path.
+     *
+     * @return Vector2f
+     */
     Vector2f getNextLocation();
+	
+    /**
+     * @brief gets the length of the path.
+     * 
+	 * gets the current length of the path.
+     *
+     * @return int
+     */	
     int getPathLength();
 
   private:
