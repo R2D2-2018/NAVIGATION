d99c078006262a458c0322bb2231fadcef09b0dd
Renske Kuip <renske.kuip@student.hu.nl>
2018-06-04 15:29:40 +0200

    Updated test files

--- Sources.cmake
+++ Sources.cmake
@@ -11,6 +11,9 @@ include_directories (${catch}/single_include)
 # Source Files:
 
 set (sources
-	src/wrap-hwlib.cpp
+	src/navigation_engine.cpp
+    src/navigation_path.cpp
+    src/wrap-hwlib.cpp
     src/libc-stub.cpp
+    src/mpu9250_interface.cpp
 )
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,63 +1,30 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 
+#include "../src/navigation_path.hpp"
 #include "wrap-hwlib.hpp"
 
-#include "some-controller.hpp"
+TEST_CASE("Navigation Path in/out test", "[navigation_path]") {
+    // Preconditions:
+    NavigationPath path;
+    path.addPathLocation({1, 5});
+    path.addPathLocation({10, 5});
+    path.addPathLocation({-1, -8});
+    path.addPathLocation({99.1, 101.2});
 
-TEST_CASE("Virtual hwlib uart") {
-    hwlib::cout << "Hello world!" << hwlib::endl;
-}
+    // Run Test:
+    auto testLocation1 = path.getNextLocation();
+    auto testLocation2 = path.getNextLocation();
+    auto testLocation3 = path.getNextLocation();
+    auto testLocation4 = path.getNextLocation();
 
-TEST_CASE("Virtual controller test demo") {
-    hwlib::test::pin_in<14 * 8> test_pin_in{// some input data
-                                            0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1,
-                                            1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1,
-                                            0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1,
-                                            0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    SomeController controller;
-
-    char buffer[256];
-
-    controller.read_string_from_pin_in(test_pin_in, buffer);
-
-    REQUIRE(strcmp(buffer, "Hello world!") == 0);
-}
-
-TEST_CASE("Virtualized hwlib::pin_in") {
-    hwlib::test::pin_in<8> test_pin_in{1, 0, 1, 0, 1, 0, 1, 1};
-
-    REQUIRE(test_pin_in.get() == 1);
-    REQUIRE(test_pin_in.get() == 0);
-    REQUIRE(test_pin_in.get() == 1);
-    REQUIRE(test_pin_in.get() == 0);
-    REQUIRE(test_pin_in.get() == 1);
-    REQUIRE(test_pin_in.get() == 0);
-    REQUIRE(test_pin_in.get() == 1);
-    REQUIRE(test_pin_in.get() == 1);
-}
-
-TEST_CASE("Virtualized hwlib::pin_out") {
-    hwlib::test::pin_out<8> test_pin_out{};
-
-    test_pin_out.set(1);
-    test_pin_out.set(0);
-    test_pin_out.set(1);
-    test_pin_out.set(1);
-    test_pin_out.set(0);
-    test_pin_out.set(0);
-    test_pin_out.set(0);
-    test_pin_out.set(1);
-
-    uint8_t *data = test_pin_out.get_data();
-
-    REQUIRE(data[0] == 1);
-    REQUIRE(data[1] == 0);
-    REQUIRE(data[2] == 1);
-    REQUIRE(data[3] == 1);
-    REQUIRE(data[4] == 0);
-    REQUIRE(data[5] == 0);
-    REQUIRE(data[6] == 0);
-    REQUIRE(data[7] == 1);
+    // Compare Postconditions:
+    REQUIRE(testLocation1.x == 1);
+    REQUIRE(testLocation1.y == 5);
+    REQUIRE(testLocation2.x == 10);
+    REQUIRE(testLocation2.y == 5);
+    REQUIRE(testLocation3.x == -1);
+    REQUIRE(testLocation3.y == -8);
+    REQUIRE(testLocation4.x == 99.1f);
+    REQUIRE(testLocation4.y == 101.2f);
 }
