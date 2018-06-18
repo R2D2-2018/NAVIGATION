0cf8c984b35339cffaf0b250647f7b2b63369778
Jeroen <jeroenvanhattem@outlook.com>
2018-06-18 12:09:07 +0200

    Added documentation for Coordinate3D class

--- src/coordinate3d.hpp
+++ src/coordinate3d.hpp
@@ -16,14 +16,78 @@ struct Coordinate3D {
     int16_t z;
 
   public:
+    /**
+     * @brief Construct a new Coordinate3D object
+     *
+     * The default constructor to create a new Coordinate3D object.
+     *
+     */
     Coordinate3D();
+
+    /**
+     * @brief Construct a new Coordinate3D object
+     *
+     * The default constructor to create a new Coordinate3D object.
+     *
+     * @param x : int16_t
+     * @param y : int16_t
+     * @param z : int16_t
+     *
+     */
     Coordinate3D(int16_t x, int16_t y, int16_t z);
+
+    /**
+     * @brief Set X value
+     *
+     * Change the X value of the object.
+     *
+     * @param value : int16_t
+     */
     void setX(int16_t value);
+
+    /**
+     * @brief Set Y value
+     *
+     * Change the Y value of the object.
+     *
+     * @param value : int16_t
+     */
     void setY(int16_t value);
+
+    /**
+     * @brief Set Z value
+     *
+     * Change the Z value of the object.
+     *
+     * @param value : int16_t
+     */
     void setZ(int16_t value);
 
+    /**
+     * @brief Return the X value
+     *
+     * Return the X value of this object.
+     *
+     * @return int16_t
+     */
     int16_t getX();
+
+    /**
+     * @brief Return the Y value
+     *
+     * Return the Y value of this object.
+     *
+     * @return int16_t
+     */
     int16_t getY();
+
+    /**
+     * @brief Return the Z value
+     *
+     * Return the Z value of this object.
+     *
+     * @return int16_t
+     */
     int16_t getZ();
 };
 
