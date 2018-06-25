a6230e84ea8a3267e5e0e356e73ddf6dd8467b1f
Jeroen <jeroenvanhattem@outlook.com>
2018-06-25 13:51:50 +0200

    It's now possible to get a polar coordinate from the Path class, which tells the robot how it should move

--- src/path.cpp
+++ src/path.cpp
@@ -1,25 +1,43 @@
 #include "path.hpp"
 
-Path::Path() {
+Path::Path(MPU9250Interface mpu9250) : mpu9250(mpu9250) {
 }
 
 PolarVector2f Path::convertCartesianToPolar(Vector2f node) {
     PolarVector2f polar;
     double angle;
     double distance;
-    angle = atan(node.getY() / node.getX());
-    distance = sqrt((pow(node.getX(), 2), pow(node.getY(), 2)));
+
+    angle = m.atan(node.getY() / node.getX());
+    distance = m.sqrt((m.pow(node.getX(), 2), m.pow(node.getY(), 2)));
     polar.setAngle(angle);
     polar.setDistance(distance);
+
+    return polar;
 }
 
-double Path::calculateToGoDirection(Vector2f node) {
-    PolarVector2f polar = convertCartesianToPolar(node);
-    // float deltaX = positionX - node.getX();
-    // float deltaY = positionY - node.getY();
+float Path::getRotationFromMPU() {
+    float rotation;
+    Coordinate3D<float> rotation3D = mpu9250.getMagnetometerValues();
+    rotation = rotation3D.getY();
+    return rotation;
+}
 
+PolarVector2f Path::getDirections(Vector2f currentPosition, Vector2f nextPosition) {
     // 1. Get current rotation in degrees.
     // 2. Calculate the difference between the desired rotation and the current rotation from the compass (in degrees).
     // 3. Get current location from something.
     // 4. Calculate the difference between desired location and current location.
+    PolarVector2f directions;
+
+    float deltaX = nextPosition.getX() - currentPosition.getX();
+    float deltaY = nextPosition.getY() - currentPosition.getY();
+
+    float angle = m.atan(deltaY / deltaX) * 180 / m.pi;
+    float distance = m.sqrt(m.pow(deltaX, 2) + m.pow(deltaY, 2));
+
+    directions.setAngle(angle);
+    directions.setDistance(distance);
+
+    return directions;
 }
--- src/path.hpp
+++ src/path.hpp
@@ -1,7 +1,7 @@
 #ifndef PATH_HPP
 #define PATH_HPP
 
-#include "math.h"
+#include "math.hpp"
 #include "mpu9250_interface.hpp"
 #include "polar_vector2f.hpp"
 #include "vector2f.hpp"
@@ -13,15 +13,49 @@
  */
 class Path {
   private:
+    Math m;
     double positionX;
     double positionY;
     double compassDegrees; // Value from 0 to 359, this resembles the degree value of a compass.
     double toGoDirection;  // Value from 0 to 359, this resembles the degree value of a compass.
+    MPU9250Interface mpu9250;
 
   public:
-    Path();
+    /**
+     * @brief Construct a new Path object
+     *
+     * @param mpu9250
+     */
+    Path(MPU9250Interface mpu9250);
+    /**
+     * @brief Convert a Cartesian coordinate to a polar coordinate
+     *
+     * This function converts a cartesian coordinate (a coordinate in the X, Y coordinate system) to a polar coordinate( a
+     * coordinate in the angle, distance system)
+     *
+     * @param node
+     * @return PolarVector2f
+     */
     PolarVector2f convertCartesianToPolar(Vector2f node);
-    double calculateToGoDirection(Vector2f node);
+    /**
+     * @brief Get direction to the next node
+     *
+     * This function calculates the difference between the current and the next node. This will be converted into a polar
+     * coordinate. It returns an angle and a distance in a PolarVector2f object.
+     *
+     * @param currentPosition
+     * @param nextPosition
+     * @return PolarVector2f
+     */
+    PolarVector2f getDirections(Vector2f currentPosition, Vector2f nextPosition);
+    /**
+     * @brief Get the Rotation from MPU object
+     *
+     * This function gets the rotation from the MPU compass.
+     *
+     * @return float
+     */
+    float getRotationFromMPU();
 };
 
 #endif // PATH_HPP
