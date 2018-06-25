32bad8f449cdfa634f43e010b4b7c62d6bf342e1
Jeroen <jeroenvanhattem@outlook.com>
2018-06-25 15:30:37 +0200

    Wrote tests for PolarVector2f and Path

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,71 +1,51 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include "../src/coordinate3d.hpp"
+#include "../src/mpu9250_interface.hpp"
 #include "../src/navigation_path.hpp"
+#include "../src/polar_vector2f.hpp"
 #include "catch.hpp"
 
-TEST_CASE("Example Test Case") {
-    REQUIRE(10 == 10);
+TEST_CASE("PolarVector2f get angle") {
+    PolarVector2f polar;
+
+    polar.setAngle(30.0);
+
+    REQUIRE(polar.getAngle() == 30);
 }
 
-TEST_CASE("Coordinates 3D get coordinates positive") {
-    Coordinate3D<int> coordinates(140, 150, 130);
+TEST_CASE("PolarVector2f get distance") {
+    PolarVector2f polar;
 
-    REQUIRE(coordinates.getX() == 140);
-    REQUIRE(coordinates.getY() == 150);
-    REQUIRE(coordinates.getZ() == 130);
+    polar.setDistance(50.0);
+
+    REQUIRE(polar.getDistance() == 50);
 }
 
-TEST_CASE("Coordinates 3D get coordinates negative") {
-    Coordinate3D<int> coordinates(-140, -150, -130);
+TEST_CASE("Path get directions") {
+    auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
+    auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
+    const int8_t address = 0x68;
 
-    REQUIRE(coordinates.getX() == -140);
-    REQUIRE(coordinates.getY() == -150);
-    REQUIRE(coordinates.getZ() == -130);
+    MPU9250Interface sensor(scl, sda, address);
+    Path path(sensor);
+
+    PolarVector2f goTo = path.getDirections(Vector2f(2, 4), Vector2f(4, 8));
+    float goToAngle = goTo.getAngle();
+
+    REQUIRE(polar.getAngle() == 4.4721);
 }
 
-TEST_CASE("Coordinates 3D set coordinates positive") {
-    Coordinate3D<int> coordinates;
-    coordinates.setX(140);
-    coordinates.setY(150);
-    coordinates.setZ(130);
+/// This one is not working, because math.h isn't working accordingly
+// TEST_CASE("Path get directions") {
+//     auto scl = hwlib::target::pin_oc(hwlib::target::pins::scl);
+//     auto sda = hwlib::target::pin_oc(hwlib::target::pins::sda);
+//     const int8_t address = 0x68;
 
-    REQUIRE(coordinates.getX() == 140);
-    REQUIRE(coordinates.getY() == 150);
-    REQUIRE(coordinates.getZ() == 130);
-}
+//     MPU9250Interface sensor(scl, sda, address);
+//     Path path(sensor);
 
-TEST_CASE("Coordinates 3D set coordinates negative") {
-    Coordinate3D<int> coordinates;
-    coordinates.setX(-140);
-    coordinates.setY(-150);
-    coordinates.setZ(-130);
+//     PolarVector2f goTo = path.getDirections(Vector2f(2, 4), Vector2f(4, 8));
+//     float goToDistance = goTo.getDistance();
 
-    REQUIRE(coordinates.getX() == -140);
-    REQUIRE(coordinates.getY() == -150);
-    REQUIRE(coordinates.getZ() == -130);
-}
-
-// TEST_CASE("Navigation Path in/out test", "[navigation_path]") {
-//     // Preconditions:
-//     NavigationPath path;
-//     path.addPathLocation({1, 5});
-//     path.addPathLocation({10, 5});
-//     path.addPathLocation({-1, -8});
-//     path.addPathLocation({99.1, 101.2});
-
-//     // Run Test:
-//     auto testLocation1 = path.getNextLocation();
-//     auto testLocation2 = path.getNextLocation();
-//     auto testLocation3 = path.getNextLocation();
-//     auto testLocation4 = path.getNextLocation();
-
-//     // Compare Postconditions:
-//     REQUIRE(testLocation1.x == 1);
-//     REQUIRE(testLocation1.y == 5);
-//     REQUIRE(testLocation2.x == 10);
-//     REQUIRE(testLocation2.y == 5);
-//     REQUIRE(testLocation3.x == -1);
-//     REQUIRE(testLocation3.y == -8);
-//     REQUIRE(testLocation4.x == 99.1f);
-//     REQUIRE(testLocation4.y == 101.2f);
+//     REQUIRE(polar.getDistance() == 63.436);
 // }
